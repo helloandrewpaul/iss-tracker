f833324aa58175f47329759e63f5198bb632d144
“helloandrewpaul” <mcgowanandrew@gmail.com>
2021-07-12 21:50:21 -0400

    interval added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React ,{useEffect,useState}from "react"
 function App() {
+
 const [issLocation, setIssLocation]=useState([])
+
 const getIssLocation =async ()=>{
 return fetch("http://api.open-notify.org/iss-now.json")
 .then((response)=> response.json())
 .then((data)=> {
   let locationArray = Object.values(data)[2]
-  // let newArray = Object.values(locationArray)[0]
-  // console.log(locationArray)
-
-  // setIssLocation(newArray)
   setIssLocation(locationArray)
-
-console.log(locationArray)
 })
 }
+
 useEffect(()=>{
-  getIssLocation()
+  const interval = setInterval(()=>{
+    getIssLocation()
+  },2000);
+  return ()=> clearInterval(interval)
+  // getIssLocation()
 },[])
-// useEffect(()=>{
-//   fetch("http://api.open-notify.org/iss-now.json",{method:"GET"})
-//   .then(())
-// })
+
 
   return (<><div>{issLocation.latitude}
   </div>
