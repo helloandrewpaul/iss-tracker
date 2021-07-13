f4ec922ed487364ecb27fe0357f279034ffac697
“helloandrewpaul” <mcgowanandrew@gmail.com>
2021-07-13 11:43:50 -0400

    clean up

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import bg from "./assets/bg.mp4"
-const Homepage = (className) => {
+import bg from "./assets/bg.mp4";
+
+const Homepage = () => {
   const [issLocation, setIssLocation] = useState([]);
 
   const getIssLocation = async () => {
@@ -13,7 +14,6 @@ const Homepage = (className) => {
       });
   };
 
-
   useEffect(() => {
     const interval = setInterval(() => {
       getIssLocation();
@@ -23,38 +23,44 @@ const Homepage = (className) => {
 
   return (
     <>
-      <Wrap className={className}>
-        <Background autoPlay loop muted src={bg} type="video/mp4"/>
-        <Title>ISS Tracker</Title>
-        <Coordinates className="long">
-          Longitude: {issLocation.latitude}
-        </Coordinates>
-        <Coordinates className="lat">
-          Latitude: {issLocation.longitude}
-        </Coordinates>
+      <Wrap>
+        <Background autoPlay loop muted src={bg} type="video/mp4" />
+        <TextWrap>
+          <Title>ISS Tracker</Title>
+          <Coordinates>Longitude: {issLocation.latitude}</Coordinates>
+          <Coordinates>Latitude: {issLocation.longitude}</Coordinates>
+        </TextWrap>
       </Wrap>
     </>
   );
 };
 
-const Background = styled.video`
-width:100%;
-height:100%;
-position:fixed;
-object-fit:cover;`
-const Title = styled.div`
-  font-size: 10vw;
-  padding: 15px;
-  margin:30px auto;
-  position: relative;
-  z-index:100;
-`;
-const Wrap = styled.div`
+const TextWrap = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
   flex-direction: column;
-  /* justify-content: center; */
+  background: rgba(0, 0, 0, 0.7);
+`;
+
+const Background = styled.video`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  object-fit: cover;
+`;
+
+const Title = styled.div`
+  font-size: 10vw;
+  padding: 15px;
+  margin: 30px auto;
+  position: relative;
+  z-index: 100;
+`;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
   animation: fadein 1s ease-out;
   @keyframes fadein {
     0% {
@@ -67,11 +73,11 @@ const Wrap = styled.div`
 `;
 
 const Coordinates = styled.div`
-margin: 0 auto;
+  margin: 0 auto;
   font-size: 5vw;
   padding: 15px;
   position: relative;
-  z-index:100;
+  z-index: 100;
 `;
 
 export default Homepage;
